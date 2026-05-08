dab3a4013a68056243457c1a6a2a863c0a7b8e10
Patrick Covil <covilpiano@gmail.com>
2026-05-08 00:51:01 -0400

    Major Rework of Client

--- client/EnderTP_Client.lua
+++ client/EnderTP_Client.lua
@@ -1,10 +1,109 @@
+local pos = term.setCursorPos
 local cls = term.clear
+local box = paintutils.drawFilledBox
+local outline = paintutils.drawBox
+local line = paintutils.drawLine 
+local tCol = term.setTextColor
+local bCol = term.setBackgroundColor
+local rsget = rs.getAnalogInput
+local rsset = rs.setAnalogOutput
+ 
 
+
+x,y = term.getSize()
+curr_state = 0
+tp_state = {
+    tp1 = nil,
+    tp2 = nil,
+    tp3 = nil
+}
+ 
 peripheral.find("modem",rednet.open)
 tp_server = rednet.lookup("tp_server","tp_host")
 
-function tpClient()
-    print("TP Request Sent")
+
+function init()
+    local user_data = {
+        username = nil,
+        tp1 = nil,
+        tp2 = nil,
+        tp3 = nil,
+    }
+    local filename = "user_data.txt"
+
+    if fs.exists(filename) then
+        user_data = loadData()
+        textutils.slowWrite("User Data Loaded from File.",printRate) 
+    else
+        textutils.slowWrite("Enter Your User: ",printRate)
+        user_data.username = read()
+        
+        saveData(user_data)
+        textutils.slowWrite("Saved to ./"..filename.."\n",printRate)
+    end
+    os.setComputerLabel(user_data.username)
+    os.sleep(1)
+end
+
+function checkValidTP()
+    while true do
+        local user_data = loadData()
+
+        if user_data.tp1 then
+            if not rednet.lookup("tp_server",user_data.tp1) then
+                tp_state.tp1 = "not_avail"
+            else
+                tp_state.tp1 = "avail"
+            end
+        else
+            tp_state.tp1 = nil
+        end
+
+        if user_data.tp2 then
+            if not rednet.lookup("tp_server",user_data.tp2) then
+                tp_state.tp2 = "not_avail"
+            else
+                tp_state.tp2 = "avail"
+            end
+        else
+            tp_state.tp2 = nil
+        end
+
+        if user_data.tp3 then
+            if not rednet.lookup("tp_server",user_data.tp3) then
+                tp_state.tp3 = "not_avail"
+            else
+                tp_state.tp3 = "avail"
+            end
+        else
+            tp_state.tp3 = nil
+        end
+
+        if curr_state == 0 then drawHomeMenu() end
+        os.sleep(0.5)
+    end
+end
+
+function saveData(data)
+    local file = fs.open("user_data.txt", "w")
+    
+    local serializedData = textutils.serialize(data) 
+    file.write(serializedData)
+    file.close()
+end
+
+function loadData()
+    if not fs.exists("user_data.txt") then return nil end
+    
+    local file = fs.open("user_data.txt", "r")
+    local content = file.readAll()
+    file.close()
+    
+    return textutils.unserialize(content)
+end
+
+function sendRequest()
+    textutils.slowWrite("TP Request Sent",printRate)
     os.sleep(2)
     rednet.send(tp_server,true,"tp_request")
     
@@ -12,6 +111,211 @@ function tpClient()
     cls()
     term.setCursorPos(1,1)
 end
+ 
+function drawEntry(height,bcolor)
+    bcolor = bcolor or colors.gray
 
-cls()
-tpClient()
+    box(8,height,x-8,height+3,bcolor)
+    line(8,height,x-8,height,colors.lightGray)
+    line(x-10,height,x-8,height,colors.lightGray)
+    
+    pos(x-9,height)
+    tCol(colors.gray)
+    bCol(colors.lightGray)
+    write("x")
+end
+
+function centerPrint(printstr,height)
+    printstr = string.sub(printstr,1,9)
+    local str_len = string.len(printstr)
+
+    pos(math.ceil((x-str_len)/2),height)
+    write(printstr)
+end
+
+function drawHomeMenu()
+    local user_data = loadData()
+    local printstr = ""
+    local bcolor
+
+    pos(1,1)
+    box(1,1,x,y,colors.black) -- Background
+ 
+    tCol(colors.orange)
+    bCol(colors.black)
+    centerPrint("TP Menu",3)
+
+    if not user_data.tp1 then
+        drawEntry(5)
+        tCol(colors.lightGray)
+        bCol(colors.gray)
+        printstr = "[Unset]"
+    else
+        printstr = user_data.tp1
+        if tp_state.tp1 == "not_avail" then
+            bcolor = colors.red
+            drawEntry(5,bcolor)
+            tCol(colors.white)
+            bCol(colors.red)
+        elseif tp_state.tp1 == "avail" then
+            bcolor = colors.green
+            drawEntry(5,bcolor)
+            tCol(colors.white)
+            bCol(colors.green)
+        else
+            drawEntry(5)
+            tCol(colors.lightGray)
+            bCol(colors.gray)
+        end
+    end
+    centerPrint(printstr,7)
+
+    if not user_data.tp2 then
+        drawEntry(10)
+        tCol(colors.lightGray)
+        bCol(colors.gray)
+        printstr = "[Unset]"
+    else
+        printstr = user_data.tp2
+        if tp_state.tp2 == "not_avail" then
+            bcolor = colors.red
+            drawEntry(10,bcolor)
+            tCol(colors.white)
+            bCol(colors.red)
+        elseif tp_state.tp2 == "avail" then
+            bcolor = colors.green
+            drawEntry(10,bcolor)
+            tCol(colors.white)
+            bCol(colors.green)
+        else
+            drawEntry(10)
+            tCol(colors.lightGray)
+            bCol(colors.gray)
+        end
+    end
+    centerPrint(printstr,12)
+
+    if not user_data.tp3 then
+        drawEntry(15)
+        tCol(colors.lightGray)
+        bCol(colors.gray)
+        printstr = "[Unset]"
+    else
+        printstr = user_data.tp3
+        if tp_state.tp2 == "not_avail" then
+            bcolor = colors.red
+            drawEntry(15,bcolor)
+            tCol(colors.white)
+            bCol(colors.red)
+        elseif tp_state.tp3 == "avail" then
+            bcolor = colors.green
+            drawEntry(15,bcolor)
+            tCol(colors.white)
+            bCol(colors.green)
+        else
+            drawEntry(15)
+            tCol(colors.lightGray)
+            bCol(colors.gray)
+        end
+    end
+    centerPrint(printstr,17)
+end
+
+function homeMenuLoop()
+    drawHomeMenu()
+    while true do 
+        local event, button, mx, my = os.pullEvent()
+ 
+        if event == "mouse_click" then 
+            curr_state = 1
+            if mx >= 8 and mx <= x-8 and my >= 6 and my <= 8 and button == 1 then
+                local user_data = loadData()
+
+                if not user_data.tp1 then 
+
+                    box(1,1,x,y,colors.black)
+                    pos(1,1)
+                    tCol(colors.orange)
+                    bCol(colors.black)
+
+                    textutils.slowWrite("Enter TP 1 Name \n[max 9 chars]: ")
+                    tCol(colors.white)
+                    user_data.tp1 = string.sub(read(),1,9)
+
+                    saveData(user_data)
+                else
+                    -- Send TP Request
+                end
+            elseif mx >= x-10 and mx <= x-8 and my == 5 and button == 1 then
+                local user_data = loadData()
+                user_data.tp1 = nil
+
+                saveData(user_data)
+            elseif mx >= 8 and mx <= x-8 and my >= 11 and my <= 13 and button == 1 then
+                local user_data = loadData()
+
+                if not user_data.tp2 then 
+
+                    box(1,1,x,y,colors.black)
+                    pos(1,1)
+                    tCol(colors.orange)
+                    bCol(colors.black)
+
+                    textutils.slowWrite("Enter TP 2 Name \n[max 9 chars]: ")
+                    tCol(colors.white)
+                    user_data.tp2 = string.sub(read(),1,9)
+
+                    saveData(user_data)
+                else
+                    -- Send TP Request
+                end
+            elseif mx >= x-10 and mx <= x-8 and my == 10 and button == 1 then
+                local user_data = loadData()
+                user_data.tp2 = nil
+
+                saveData(user_data)
+            elseif mx >= 8 and mx <= x-8 and my >= 16 and my <= 18 and button == 1 then
+                local user_data = loadData()
+
+                if not user_data.tp3 then 
+
+                    box(1,1,x,y,colors.black)
+                    pos(1,1)
+                    tCol(colors.orange)
+                    bCol(colors.black)
+
+                    textutils.slowWrite("Enter TP 3 Name \n[max 9 chars]: ")
+                    tCol(colors.white)
+                    user_data.tp3 = string.sub(read(),1,9)
+
+                    saveData(user_data)
+                else
+                    -- Send TP Request
+                end
+            elseif mx >= x-10 and mx <= x-8 and my == 15 and button == 1 then
+                local user_data = loadData()
+                user_data.tp3 = nil
+
+                saveData(user_data)
+            end
+            curr_state = 0
+            drawHomeMenu()
+        end
+    end
+end
+
+
+
+function main()
+    cls()
+    pos(1,1)
+    while true do
+        if curr_state == 0 then
+            cls()
+            homeMenuLoop()
+        end
+    end
+end
+
+init()
+parallel.waitForAll(main,checkValidTP)
--- server/EnderTP_Server.lua
+++ server/EnderTP_Server.lua
@@ -10,11 +10,59 @@ printRate = 35
 peripheral.find("modem",rednet.open)
 rsset("front",15)
 
-function tpServer(comp_label)
+function init()
+    local server_data = {
+        username = nil,
+        servername = nil
+    }
+    local filename = "server_data.txt"
+
+    if fs.exists(filename) then
+        server_data = loadData()
+        textutils.slowWrite("Data Loaded from File.",printRate) 
+    else
+        textutils.slowWrite("Enter The TP Name: ",printRate)
+        server_data.servername = read()
+
+        textutils.slowWrite("Enter Your User: ",printRate)
+        server_data.username = read()
+        
+        saveData(server_data)
+        textutils.slowWrite("Saved to ./"..filename.."\n",printRate)
+    end
+    os.setComputerLabel(server_data.servername)
+    rednet.host("tp_request",server_data.servername)
+    os.sleep(2)
+
+    cls()
+    term.setCursorPos(1,1)
+
+    return server_data.servername,server_data.username
+end
+
+function saveData(data)
+    local file = fs.open("server_data.txt", "w")
+    
+    local serializedData = textutils.serialize(data) 
+    file.write(serializedData)
+    file.close()
+end
+
+function loadData()
+    if not fs.exists("server_data.txt") then return nil end
+    
+    local file = fs.open("server_data.txt", "r")
+    local content = file.readAll()
+    file.close()
+    
+    return textutils.unserialize(content)
+end
+
+function tpServer(comp_label,user)
     while true do
         textutils.slowWrite("Waiting for TP Request..\n\n",printRate)
         local target_id,message,protocol = rednet.receive()
-        textutils.slowWrite("Recieved Message: "..target_id..message.."\n",printRate)
+        textutils.slowWrite("Recieved Message: "..target_id,printRate)
         
         if protocol == "tp_request" and message == comp_label then
             textutils.slowWrite("Message is a TP Request!\n",printRate)
@@ -28,37 +76,6 @@ function tpServer(comp_label)
             textutils.slowWrite("Invalid Request\n",printRate)
         end
     end
-end
-
-function init()
-    local cname
-    local filename = "params.txt"
-
-    if fs.exists(filename) then
-        local file = fs.open(filename, "r")
-        cname = file.readLine()
-        file.close()
-        textutils.slowWrite("TP Name Loaded from File: " ..cname.."\n",printRate) 
-    else
-        textutils.slowWrite("Enter TP Name: ",printRate)
-        cname = read()
-
-        local file = fs.open(filename, "w")
-        file.writeLine(cname)
-        file.close()
-
-        textutils.slowWrite("Saved to ./"..filename.."\n",printRate)
-    end
-
-    os.sleep(2)
-
-    cls()
-    term.setCursorPos(1,1)
-    
-    os.setComputerLabel(comp_label)
-    rednet.host("tp_request",cname)
-
-    return cname
 end
 
 
