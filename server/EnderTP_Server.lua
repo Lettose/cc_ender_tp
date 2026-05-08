b8be279f8901703755104a208669ba66b576f903
Patrick Covil <covilpiano@gmail.com>
2026-05-07 20:40:05 -0400

    Server Adjustments - Params R/W

--- server/EnderTP_Server.lua
+++ server/EnderTP_Server.lua
@@ -2,34 +2,64 @@ local rsget = rs.getAnalogInput
 local rsset = rs.setAnalogOutput
 local cls = term.clear
 
-comp_label = "tp_host"
-protocol = "tp_server"
+cls()
+term.setCursorPos(1,1)
 
-os.setComputerLabel(comp_label)
+printRate = 35
+
 peripheral.find("modem",rednet.open)
-rednet.host(protocol,comp_label)
+rsset("front",15)
 
-rsset("back",15)
-
-function tpServer()
+function tpServer(comp_label)
     while true do
+        textutils.slowWrite("Waiting for TP Request..\n\n",printRate)
         local target_id,message,protocol = rednet.receive()
-        print("Recieved Message: ",target_id,message)
+        textutils.slowWrite("Recieved Message: "..target_id..message.."\n",printRate)
         
-        if protocol == "tp_request" and message == true then
-            print("Message is a Teleport Request")
-            print("Triggering Teleport..")
+        if protocol == "tp_request" and message == comp_label then
+            textutils.slowWrite("Message is a TP Request!\n",printRate)
 
-            rsset("back",0)
+            rsset("front",0)
             os.sleep(2)
-            rsset("back",15)
+            rsset("front",15)
             
-            print("Done!")
+            textutils.slowWrite("Done!\n",printRate)
         else
-            print("Invalid Request")
+            textutils.slowWrite("Invalid Request\n",printRate)
         end
     end
 end
 
-cls()
-tpServer()
+function init()
+    local cname
+    local filename = "params.txt"
+
+    if fs.exists(filename) then
+        local file = fs.open(filename, "r")
+        cname = file.readLine()
+        file.close()
+        textutils.slowWrite("TP Name Loaded from File: " ..cname.."\n",printRate) 
+    else
+        textutils.slowWrite("Enter TP Name: ",printRate)
+        cname = read()
+
+        local file = fs.open(filename, "w")
+        file.writeLine(cname)
+        file.close()
+
+        textutils.slowWrite("Saved to ./"..filename.."\n",printRate)
+    end
+
+    os.sleep(2)
+
+    cls()
+    term.setCursorPos(1,1)
+    
+    os.setComputerLabel(comp_label)
+    rednet.host("tp_request",cname)
+
+    return cname
+end
+
+
+tpServer(init())
